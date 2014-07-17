21c389a8230886a8192fe56b6df284a9389ce073
jordantang <jordan.tang@anki.com>
2014-07-17 15:08:59 -0700

    Tab parsing
    Fixed tab parsing

--- Java_Build/sdk_ui/ANKI_sdk_test.py
+++ Java_Build/sdk_ui/ANKI_sdk_test.py
@@ -2,8 +2,12 @@
 import SDK.py
 def main():
 	ANKI.ANKI_start()
-	if scan_cars('enter string here', 99) < 99:
-		scan_cars('enter string here', 99)
-		#end
+	scan_cars('enter string here', 66)
+		if scan_cars('enter string here', 66) < 9:
+		change_lane('enter string here', 55, 66)
+		for a in range(1,9):
+			connect_car('enter string here', 'enter string here')
+			#end
+			#end
 	ANKI.ANKI_end()
 main()
