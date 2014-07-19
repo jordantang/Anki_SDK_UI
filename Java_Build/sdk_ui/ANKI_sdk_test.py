413a46e142fd8ee1b6bd3efd058c5e9717bef630
jordantang <tennisswimmer@gmail.com>
2014-07-18 21:27:47 -0700

    Metadata from home?
    Metadata from home

--- Java_Build/sdk_ui/ANKI_sdk_test.py
+++ Java_Build/sdk_ui/ANKI_sdk_test.py
@@ -2,8 +2,8 @@
 import SDK.py
 def main():
 	ANKI.ANKI_start()
-	if scan_cars('enter string here', 99) < 99:
-		scan_cars('enter string here', 99)
-		#end
+	if scan_cars('enter string here', 1) > 1:
+		connect_car('enter string here', 'enter string here')
+		scan_cars('enter string here', 99#end
 	ANKI.ANKI_end()
 main()
